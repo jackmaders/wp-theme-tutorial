1948408022c0be61d71ee82d4219aab2fa913028
Jack Maders <jackwmaders@gmail.com>
2024-02-12 13:24:32 +0000

    add link to title

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -3,10 +3,14 @@
         <div class="media">
             <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?= the_post_thumbnail_url('thumbnail') ?>" alt="image">
             <div class="media-body">
-                <h3 class="title mb-1"><a href="post.html"><?= the_title() ?></a></h3>
+                <h3 class="title mb-1">
+                    <a href="<?= the_permalink() ?>"><?= the_title() ?></a>
+                </h3>
                 <div class="meta mb-1">
                     <span class="date"><?= the_date() ?></span>
-                    <span class="comment"><a href="#"><?= comments_number() ?></a></span>
+                    <span class="comment">
+                        <a href="#"><?= comments_number() ?></a>
+                    </span>
                 </div>
                 <div class="intro"><?= the_excerpt() ?></div>
                 <a class="more-link" href="<?= the_permalink() ?>">Read more &rarr;</a>
