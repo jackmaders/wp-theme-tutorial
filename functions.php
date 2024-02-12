9dd34194e4fde6f110ce1d72117254337c70c149
Jack Maders <jackwmaders@gmail.com>
2024-02-12 12:33:19 +0000

    add post thiumbnails support

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ function tutorial_theme_support()
 {
     add_theme_support("title-tag");
     add_theme_support("custom-logo");
+    add_theme_support("post-thumbnails");
 }
 
 function tutorial_menus()
